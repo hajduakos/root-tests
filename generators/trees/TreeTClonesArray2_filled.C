eb530ae36b6d9cbf59e2255c8571f00b0baf8d5e
Akos Hajdu <akos.hajdu@cern.ch>
2015-08-11 10:16:24 +0200

    Finished tests with collections.

---
+++ generators/trees/TreeTClonesArray2_filled.C
@@ -0,0 +1,46 @@
+#include "TreeTClonesArray2.h"
+#include "TreeTClonesArray2.C"
+#include <TH2.h>
+#include <TStyle.h>
+
+class TreeTClonesArray2_filled : public TreeTClonesArray2{
+public:
+   TreeTClonesArray2_filled(TTree * /*tree*/ =0) : TreeTClonesArray2(0) { }
+
+   virtual void    Begin(TTree *tree);
+   virtual void    SlaveBegin(TTree *tree);
+   virtual Bool_t  Process(Long64_t entry);
+   virtual void    SlaveTerminate();
+   virtual void    Terminate();
+
+   ClassDef(TreeTClonesArray2_filled, 0);
+};
+
+void TreeTClonesArray2_filled::Begin(TTree * /*tree*/)
+{
+   TString option = GetOption();
+}
+
+void TreeTClonesArray2_filled::SlaveBegin(TTree * /*tree*/)
+{
+   TString option = GetOption();
+}
+
+Bool_t TreeTClonesArray2_filled::Process(Long64_t entry)
+{
+   fReader.SetEntry(entry);
+
+   fprintf(stderr, "fEv:");
+   for (Int_t i = 0; i < arr_fEv.GetSize(); ++i) fprintf(stderr, " %d", arr_fEv[i]);
+   fprintf(stderr, "\n");
+
+   fprintf(stderr, "fPx:");
+   for (Int_t i = 0; i < arr_fPx.GetSize(); ++i) fprintf(stderr, "% .1f", arr_fPx[i]);
+   fprintf(stderr, "\n");
+
+   return kTRUE;
+}
+
+void TreeTClonesArray2_filled::SlaveTerminate() { }
+
+void TreeTClonesArray2_filled::Terminate() { }
