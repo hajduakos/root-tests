55e4cd481d4164dff26031c927f6a90300215ca6
Akos Hajdu <akos.hajdu@cern.ch>
2015-08-11 10:31:38 +0200

    Started writing tests for classes.

---
+++ generators/trees/TreeClass0_filled.C
@@ -0,0 +1,40 @@
+#include "TreeClass0.h"
+#include "TreeClass0.C"
+#include <TH2.h>
+#include <TStyle.h>
+
+class TreeClass0_filled : public TreeClass0{
+public:
+   TreeClass0_filled(TTree * /*tree*/ =0) : TreeClass0(0) { }
+
+   virtual void    Begin(TTree *tree);
+   virtual void    SlaveBegin(TTree *tree);
+   virtual Bool_t  Process(Long64_t entry);
+   virtual void    SlaveTerminate();
+   virtual void    Terminate();
+
+   ClassDef(TreeClass0_filled, 0);
+};
+
+void TreeClass0_filled::Begin(TTree * /*tree*/)
+{
+   TString option = GetOption();
+}
+
+void TreeClass0_filled::SlaveBegin(TTree * /*tree*/)
+{
+   TString option = GetOption();
+}
+
+Bool_t TreeClass0_filled::Process(Long64_t entry)
+{
+   fReader.SetEntry(entry);
+
+   fprintf(stderr, "Ev: %d, Px: %.1f\n", ClassC_branch->GetEv(), ClassC_branch->GetPx());
+
+   return kTRUE;
+}
+
+void TreeClass0_filled::SlaveTerminate() { }
+
+void TreeClass0_filled::Terminate() { }
